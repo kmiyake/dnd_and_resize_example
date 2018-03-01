ca481ffbe53e9b12038e531a30999059bd5bd5a5
Kota Miyake <miyake.kota@gmail.com>
2018-03-01 10:45:13 +0900

    Touch event support

--- src/Container.js
+++ src/Container.js
@@ -1,13 +1,14 @@
 import React, { Component, } from "react";
 import { DragDropContextProvider } from "react-dnd";
 import HTML5Backend from 'react-dnd-html5-backend'
+import TouchBackend from 'react-dnd-touch-backend';
 import Dustbin from "./Dustbin.js";
 import Box from "./Box.js";
 
 export default class Container extends Component {
   render() {
     return (
-      <DragDropContextProvider backend={HTML5Backend}>
+      <DragDropContextProvider backend={TouchBackend}>
         <div>
           <div style={{ overflow: 'hidden', clear: 'both' }}>
             <Dustbin />
